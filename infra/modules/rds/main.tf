8056f99c70cdea257f2ea806ff4fafbcd81dfa46
SHIVANI <shivani.walekar16@gmail.com>
2026-07-07 00:27:58 +0530

    Added Terraform network module

--- infra/modules/rds/main.tf
+++ infra/modules/rds/main.tf
@@ -1,15 +1,15 @@
 resource "aws_db_instance" "postgres" {
 
-  identifier         = "${var.project_name}-db"
-  engine             = "postgres"
-  engine_version     = "15"
-  instance_class     = "db.t3.micro"
+  identifier     = "${var.project_name}-db"
+  engine         = "postgres"
+  engine_version = "15"
+  instance_class = "db.t3.micro"
 
-  allocated_storage  = 20
+  allocated_storage = 20
 
-  db_name            = var.db_name
-  username           = var.db_username
-  password           = var.db_password
+  db_name  = var.db_name
+  username = var.db_username
+  password = var.db_password
 
   skip_final_snapshot = true
 
