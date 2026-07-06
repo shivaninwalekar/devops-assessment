5e8fb7bf183e7593daaec8bfa3a9058fee77d63b
SHIVANI <shivani.walekar16@gmail.com>
2026-07-07 00:18:16 +0530

    Added Terraform network module

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -1,7 +1,113 @@
 resource "aws_vpc" "main" {
+
   cidr_block = var.vpc_cidr
 
   tags = {
     Name = "${var.project_name}-vpc"
   }
-}# Network module
+
+}
+resource "aws_vpc" "main" {
+
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+
+}
+
+resource "aws_subnet" "public_1" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.public_subnet_1
+
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "Public-Subnet-1"
+  }
+
+}
+
+resource "aws_subnet" "public_2" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.public_subnet_2
+
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "Public-Subnet-2"
+  }
+
+}
+
+resource "aws_subnet" "private_1" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.private_subnet_1
+
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "Private-Subnet-1"
+  }
+
+}
+
+resource "aws_subnet" "private_2" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.private_subnet_2
+
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "Private-Subnet-2"
+  }
+
+}
+resource "aws_subnet" "public_2" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.public_subnet_2
+
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "Public-Subnet-2"
+  }
+
+}
+resource "aws_subnet" "private_1" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.private_subnet_1
+
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "Private-Subnet-1"
+  }
+
+}
+resource "aws_subnet" "private_2" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.private_subnet_2
+
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "Private-Subnet-2"
+  }
+
+}
--- infra/modules/network/variables.tf
+++ infra/modules/network/variables.tf
@@ -1,9 +1,25 @@
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-}
-
 variable "project_name" {
   description = "Project Name"
   type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+}
+
+variable "public_subnet_1" {
+  type = string
+}
+
+variable "public_subnet_2" {
+  type = string
+}
+
+variable "private_subnet_1" {
+  type = string
+}
+
+variable "private_subnet_2" {
+  type = string
 }
